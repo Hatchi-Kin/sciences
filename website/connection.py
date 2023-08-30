728e1d5bb2803a7390607cf6c8da27fc45ec74e7
Hatchi-Kin <130794142+Hatchi-Kin@users.noreply.github.com>
2023-08-30 14:28:45 +0200

    Update connection.py

--- website/connection.py
+++ website/connection.py
@@ -6,15 +6,10 @@ class MongoAccess :
     __PW = "pass12345"
     __DB_NAME = "DBLP"
 
-
-    # @classmethod
-    # def connexion(cls) :
-    #     cls.client = MongoClient(f"mongodb://{cls.__USER}:{cls.__PW}@127.0.0.1:27017")
-    #     cls.db = cls.client[cls.__DB_NAME]
-    #     return cls.db
     
     @classmethod
     def connexion(cls):
+#        cls.client = MongoClient(f"mongodb://{cls.__USER}:{cls.__PW}@127.0.0.1:27017")
         cls.client = MongoClient(f"mongodb://{cls.__USER}:{cls.__PW}@mongo-db:27017")
         cls.db = cls.client[cls.__DB_NAME]
         return cls.db
